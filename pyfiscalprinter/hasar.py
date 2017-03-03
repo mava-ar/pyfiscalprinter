08c9930ec3050d7c5eda35c7cf164da895c2b4ff
Ayelen Guerra <ayelen.guerra117@gmail.com>
2017-03-03 12:11:43 -0300

    * Se adapta la descripcion larga del item para impresora Hasar.

--- pyfiscalprinter/hasar.py
+++ pyfiscalprinter/hasar.py
@@ -359,8 +359,6 @@ class HasarPrinter(PrinterInterface):
 
     def addItem(self, description, quantity, price, iva, discount, discountDescription, negative=False,
                 long_description=False):
-        if type(description) in types.StringTypes:
-            description = [description]
         if negative:
             sign = 'm'
         else:
@@ -369,6 +367,11 @@ class HasarPrinter(PrinterInterface):
         priceUnit = price
         priceUnitStr = str(priceUnit).replace(",", ".")
         ivaStr = str(float(iva)).replace(",", ".")
+        if long_description:
+            description = self.truncate_description(description)
+        else:
+            if type(description) in types.StringTypes:
+                description = [description]
         for d in description[:-1]:
             self._sendCommand(self.CMD_PRINT_TEXT_IN_FISCAL, [self._formatText(d, 'fiscalText'), "0"])
         reply = self._sendCommand(self.CMD_PRINT_LINE_ITEM,
@@ -513,3 +516,13 @@ class HasarPrinter(PrinterInterface):
     def close(self):
         self.driver.close()
         self.driver = None
+
+    def truncate_description(self, product_name):
+        """
+        Divide la descripción en array de n strings
+        """
+        text = formatText(product_name[:78])
+        n = 26
+        description = [text[i:i+n] for i in range(0, len(text), n)]
+
+        return description
